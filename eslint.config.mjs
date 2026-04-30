d038d48a8279bbf1ff7b94409607df1cce806c43
Zamiell <5511220+Zamiell@users.noreply.github.com>
2026-04-30 14:15:03 -0400

    chore: comment out lint check

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,12 @@ export default defineConfig(
   ...completeConfigBase,
 
   {
+    rules: {
+      // Insert changed or disabled rules here, if necessary.
+    },
+  },
+
+  {
     ignores: ["media/**"],
   },
 );
